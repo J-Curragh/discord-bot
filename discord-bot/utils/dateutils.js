59099190d373f9ddbf4c36dd6b49eccdff5a3582
J-Curragh <jcurragh03@gmail.com>
2022-03-08 22:44:24 +0000

    Add more helpful datetime related utils

--- discord-bot/utils/dateutils.js
+++ discord-bot/utils/dateutils.js
@@ -6,4 +6,27 @@ export default class {
       start + Math.random() * (end - start),
     );
   }
+
+  // Generate a random time in 24-hour format
+  static getRandomTime() {
+    const hours = Math.floor(Math.random() * 24);
+    const minutes = Math.floor(Math.random() * 60);
+    return `${hours}:${minutes}`;
+  }
+
+  // Get current time in 24-hour format
+  static getCurrentTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${hours}:${minutes}`;
+  }
+  
+  // Check two times are equal
+  static areTimesEqual(time1, time2) {
+    const { hours1, minutes1 } = time1.split(':');
+    const { hours2, minutes2 } = time2.split(':');
+    return hours1 === hours2 && minutes1 === minutes2;
+  }
+
 }
